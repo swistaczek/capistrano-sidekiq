3e50fa426043f77edcbfb902f3a70cef187fb3ab
Ernest Bursa <ernest@bzdury.pl>
2014-08-21 15:57:05 +0200

    Update capistrano2.rb

--- lib/capistrano/tasks/capistrano2.rb
+++ lib/capistrano/tasks/capistrano2.rb
@@ -59,7 +59,7 @@ Capistrano::Configuration.instance.load do
         args.push '--daemon'
       end
 
-      run "if [ -d #{current_path} ] && [ ! -f #{pid_file} ]; then cd #{current_path} ; #{fetch(:sidekiq_cmd)} #{args.compact.join(' ')} ; else echo 'Sidekiq is already running'; fi", pty: false
+      run "if [ -d #{current_path} ] && [ ! -f #{pid_file} ]; then cd #{current_path} ; #{fetch(:sidekiq_cmd)} #{args.compact.join(' ')} else echo 'Sidekiq is already running'; fi", pty: false
     end
 
     desc 'Quiet sidekiq (stop accepting new work)'
